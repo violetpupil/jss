1ad846d443bea7530e6a5cfffebacfa08b659e97
shouguang <495140158@qq.com>
2023-11-16 15:44:35 +0800

    不渲染左上角的 title 和 logo

--- lib/antd/components/pro/layout.tsx
+++ lib/antd/components/pro/layout.tsx
@@ -1,1 +1,9 @@
-// https://procomponents.ant.design/components/layout)
+// https://procomponents.ant.design/components/layout
+"use client";
+
+import { ProLayout } from "@ant-design/pro-components";
+
+export function DefaultLayout({ children }: { children: React.ReactNode }) {
+  // 不渲染左上角的 title 和 logo
+  return <ProLayout menuHeaderRender={false}>{children}</ProLayout>;
+}
